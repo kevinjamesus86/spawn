6d06b1402f8bfcdbf0809af2cce08bc74872267b
Kevin James <kevinjamesus86@gmail.com>
2015-05-28 15:22:07 -0700

    chore: update test path

--- gruntfile.js
+++ gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
     },
     watch: {
       spawn: {
-        files: ['gruntfile.js', 'src/*.js', 'tests/*.js'],
+        files: ['gruntfile.js', 'src/*.js', 'test/*.js'],
         tasks: ['default']
       }
     }
