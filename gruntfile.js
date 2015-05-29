056d9f692f491641206852eaa6a356af474c4d96
Kevin James <kevinjamesus86@gmail.com>
2015-05-29 00:42:33 -0700

    build: stuff files into dist folder, add banner

--- gruntfile.js
+++ gruntfile.js
@@ -1,22 +1,39 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     clean: {
-      spawn: ['spawn.min.js']
+      spawn: ['dist/**']
     },
     jshint: {
       spawn: {
         src: ['gruntfile.js', 'src/*.js']
       }
     },
+    copy: {
+      spawn: {
+        src: 'src/spawn.js',
+        dest: 'dist/spawn.js'
+      }
+    },
     uglify: {
       spawn: {
         files: {
-          'spawn.min.js': 'src/spawn.js'
+          'dist/spawn.min.js': 'src/spawn.js'
+        },
+        options: {
+          banner: '/**\n' +
+            ' * <%= pkg.description %>\n' +
+            ' * @version v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            ' * @author <%= pkg.author %>\n' +
+            ' * Copyright (c) <%= grunt.template.today("yyyy") %> Kevin James\n' +
+            ' * Licensed under the MIT license.\n' +
+            ' */\n'
         }
       }
     },
@@ -29,5 +46,5 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('dist', ['clean', 'jshint', 'uglify']);
+  grunt.registerTask('dist', ['clean', 'jshint', 'copy', 'uglify']);
 };
--- src/spawn.js
+++ src/spawn.js
@@ -1,3 +1,10 @@
+/**
+ * sane, flexible threading for modern browsers
+ * @version v0.1.0 - 2015-05-29
+ * @author Kevin James <kevinjamesus86@gmail.com>
+ * Copyright (c) 2015 Kevin James <kevinjamesus86@gmail.com>
+ * Licensed under the MIT license.
+ */
 (function(root, factory) {
 
   // universal module definition
